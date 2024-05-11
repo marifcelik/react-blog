b4bef4bf75e59f61ffea8630513db0de7977fc0e
arif <arif-celik56@hotmail.com>
2024-05-11 13:15:15 +0300

    fix: large build files

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -10,5 +10,22 @@ export default defineConfig({
       '@': path.resolve(__dirname, 'src'),
       '#': path.resolve(__dirname, 'src/pages')
     }
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.indexOf('node_modules') !== -1) {
+            const basic = id.toString().split('node_modules/')[1]
+            const sub1 = basic.split('/')[0].toString()
+            if (sub1 !== '.pnpm') {
+              return sub1
+            }
+            const sub2 = basic.split('/')[1]
+            return sub2.split('@')[sub2[0] === '@' ? 1 : 0].toString()
+          }
+        }
+      }
+    }
   }
 })
